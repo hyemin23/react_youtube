b72fc8fa54ea7d3fb44bbc3295ffb328c57adb7c
hyemin23 <hm_stom@naver.com>
2021-03-30 18:32:57 +0900

    #key property 추기

--- client/src/components/views/NavBar/Sections/LeftMenu.js
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -4,10 +4,10 @@ import { Menu } from 'antd';
 function LeftMenu(props) {
   return (
     <Menu mode={props.mode}>
-      <Menu.Item key="mail">
+      <Menu.Item key="home">
         <a href="/">Home</a>
       </Menu.Item>
-      <Menu.Item key="mail">
+      <Menu.Item key="subscription">
         <a href="/subscription">내 구독 페이지</a>
       </Menu.Item>
     </Menu>
--- client/src/components/views/VideoDetailPage/Sections/SideVideos.js
+++ client/src/components/views/VideoDetailPage/Sections/SideVideos.js
@@ -21,23 +21,25 @@ function SideVideos() {
         var minutes = Math.floor(video.duration / 60);
         var seconds = Math.floor(video.duration - minutes * 60);
 
-        return <div style={{ display: 'flex', marginTop: '1rem', padding: '0 2rem' }}>
-            <div style={{ width: '40%', marginRight: '1rem' }}>
-                <a href={`/video/${video._id}`} style={{ color: 'gray' }}>
-                    <img style={{ width: '100%' }} src={`http://localhost:5000/${video.thumbnail}`} alt="thumbnail" />
-                </a>
-            </div>
+        return (
+            <div key={index} style={{ display: 'flex', marginTop: '1rem', padding: '0 2rem' }}>
+                <div style={{ width: '40%', marginRight: '1rem' }}>
+                    <a href={`/video/${video._id}`} style={{ color: 'gray' }}>
+                        <img style={{ width: '100%' }} src={`http://localhost:5000/${video.thumbnail}`} alt="thumbnail" />
+                    </a>
+                </div>
 
-            <div style={{ width: '50%' }}>
-                <a href={`/video/${video._id}`} style={{ color: 'gray' }}>
-                    <span style={{ fontSize: '1rem', color: 'black' }}>{video.title}  </span><br />
-                    <span>{video.writer.name}</span><br />
-                    <span>{video.views}</span><br />
-                    <span>{minutes} : {seconds}</span><br />
-                </a>
+                <div style={{ width: '50%' }}>
+                    <a href={`/video/${video._id}`} style={{ color: 'gray' }}>
+                        <span style={{ fontSize: '1rem', color: 'black' }}>{video.title}  </span><br />
+                        <span>{video.writer.name}</span><br />
+                        <span>{video.views}</span><br />
+                        <span>{minutes} : {seconds}</span><br />
+                    </a>
+                </div>
             </div>
-        </div>
-    })
+        );
+    });
 
 
 
