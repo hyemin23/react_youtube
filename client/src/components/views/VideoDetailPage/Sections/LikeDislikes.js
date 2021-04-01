d2709b40083cc592ec4839d6b021866d3fe78a38
hyemin23 <hm_stom@naver.com>
2021-04-01 15:18:47 +0900

    좋아요 기능 구현

--- client/src/components/views/VideoDetailPage/Sections/LikeDislikes.js
+++ client/src/components/views/VideoDetailPage/Sections/LikeDislikes.js
@@ -1,4 +1,4 @@
-import { Icon, Tooltip } from 'antd'
+import { Icon, message, Tooltip } from 'antd'
 import Axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
@@ -75,12 +75,70 @@ function LikeDislikes(props) {
     }, []);
 
 
+    //손가락 버튼 클릭 시
+    const onLike = () => {
+
+        //좋아요 상태가 false면
+        //Like 상태가 클릭이 안되어 있는 상태이고, 클릭했을 때 "1" 올려줘야함.
+        if (!likeCheck) {
+            //variable 상태에 따라서 비디오,댓글 좋아요인지 판단해서 결과 값을 받아옴.
+            Axios.post("/api/like/upLike", variable).then((res) => {
+                if (res.data.success) {
+                    console.log("좋아요 올리기");
+
+                    //like 정보 1 올려주기
+                    setLikes(likes + 1);
+                    //클릭한 상태로 만들어주기
+                    setLikeCheck(true);
+
+                    //만약 싫어요(dislike)가 클릭이 되어있다면 
+                    if (DislikeCheck) {
+                        //싫어요 버튼을 클릭이 안되어있게 하고 숫자 -1 시켜주기
+                        setDislikeCheck(false);
+                        setDislikes(DislikeCheck - 1);
+
+                    }
+
+                } else {
+                    message.warning("like를 올리지 못 하였습니다.");
+                }
+            })
+        }
+        //좋아요 상태가 true라면
+        //즉,좋아요가 되어있는데 클릭을 했다면 좋아요 내리기
+        else {
+            Axios.post("/api/like/unLike", variable).then((res) => {
+                if (res.data.success) {
+                    console.log("좋아요 내리기");
+                    //좋아요 내리기
+                    setLikes(likes - 1);
+                    //클릭을 안 한 상태로 만들어주기
+                    setLikeCheck(false);
+                } else {
+
+                    message.warning("like를 내리지 못 하였습니다.")
+                }
+            })
+        }
+    }
+
+
+    //싫어요 손가락 클릭 시 
+    const onDisLike = () => {
+
+    }
+
+
+
+
     return (
         !likeCheck && !DislikeCheck && (
             <div>
                 <span key="comment-basic-like" >
                     <Tooltip title="좋아요">
-                        <Icon type="like" theme={likes ? "filled" : "outlined"} />
+                        <Icon type="like" theme={likes ? "filled" : "outlined"}
+                            onClick={onLike}
+                        />
                     </Tooltip>
                 </span>
                 <span stlyle={{ paddingLeft: "8px", cursor: "auto" }}>
@@ -89,7 +147,9 @@ function LikeDislikes(props) {
 
                 <span key="comment-basic-dislike" >
                     <Tooltip title="싫어요">
-                        <Icon type="dislike" theme={Dislikes ? "filled" : "outlined"} />
+                        <Icon type="dislike" theme={Dislikes ? "filled" : "outlined"}
+                            onClick={onDisLike}
+                        />
                     </Tooltip>
                 </span>
                 <span stlyle={{ paddingLeft: "8px", cursor: "auto" }}>
--- server/routes/like.js
+++ server/routes/like.js
@@ -61,5 +61,60 @@ router.post("/getDislikes", (req, res) => {
 });
 
 
+//좋아요 올리기
+router.post("/upLike", (req, res) => {
+    let variable = {};
+
+    //비디오 좋아요
+    if (req.body.videoId) {
+        variable = { videoId: req.body.videoId }
+    }
+    //댓글 좋아요
+    else {
+        variable = { commentId: req.body.commentId }
+    }
+
+    //Like collection에 좋아요 정보를 저장하는 과정
+    const like = new Like(variable);
+    like.save((err, likeResult) => {
+        if (err) return res.status(400).send(err);
+
+        //만약, dislike가 클릭되어있다면 dislike를 1 줄여준다.
+        Dislike.findByIdAndDelete(variable).exec((err, disLikeResult) => {
+            if (err) return res.status(400).send(400);
+            return status(200).json({
+                success: true
+            })
+        })
+    })
+});
+
+//좋아요 내리기
+router.post("/upLike", (req, res) => {
+    let variable = {};
+
+    //비디오 싫어요
+    if (req.body.videoId) {
+        variable = { videoId: req.body.videoId }
+    }
+    //댓글 싫어요
+    else {
+        variable = { commentId: req.body.commentId }
+    }
+
+    //Like collection에서 좋아요 정보 지워주기
+    Like.findByIdAndDelete(variable).exec((err, result) => {
+        if (err) return res.status(400).json({
+            success: false
+            , err
+        });
+        return res.status(200).json({
+            success: true
+            , result
+        });
+    });
+});
+
+
 module.exports = router;
 
